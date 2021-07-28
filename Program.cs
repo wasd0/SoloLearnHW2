9a0bf825f54fff9ea331320b0d4facfdbe67b788
Shvedov Kirill <84603952+wasd0@users.noreply.github.com>
2021-07-29 01:20:01 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloLearnHW2
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Создается массив из 9 слов
+            string[] words =
+                {"home", "programming", "victory", "C#", "football", "sport", "book", "learn", "dream", "fun"};
+            string letter = Console.ReadLine();
+            int count = 0;
+
+            //нам нужно пробежаться по массиву так, чтобы выводились только те слова,
+            //в которых есть буква, которую мы напишем
+            //если в словах нет той буквы, то выводим "No match"
+            for (int i = 0; count < words.Length; count++)
+            {
+                if (words[count].Contains(letter) is true) //Проверяем, есть ли в слове буква
+                {
+                    Console.WriteLine(words[count]);
+                    i++;
+                }
+                else
+                {
+                    if (i < 1 && count == words.Length-1) //если в массиве нет буквы и если в консоли ничего не было выведено
+                    {                                     //то выводится текст
+                        Console.WriteLine("No match");
+                    }
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
